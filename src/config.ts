028c5a95d5432fc62ed05b04bb8732a73213d1f9
Rafimaaktwebsites <contact@rafimaaktwebsites.nl>
2026-08-24 21:07:47 +0200

    app v1.1.1  update

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
-  WINDOWS_DOWNLOAD_URL: "https://github.com/xavitje/RhymeHelper/releases/download/V1.0.1/RymeHelper-Setup-1.0.0.exe",
+  WINDOWS_DOWNLOAD_URL: "https://github.com/xavitje/RhymeHelper/releases/download/V1.1.1/Rhyme-Helper-Setup-1.1.1.exe",
   LEMON_SQUEEZY_CHECKOUT_URL: "https://checkout.rhymehelper.store/checkout/buy/85bfb619-30c6-4b30-89c9-14830c1ce986?embed=1",
   PRICE: "€19.99",
-  SALE_PRICE: "€9.99", // Leave empty string "" if no sale
+  SALE_PRICE: "€9.99",
 };
